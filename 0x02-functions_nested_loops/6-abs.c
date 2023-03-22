63a235239ce7d6e4b266f1afaa699df6e6a341a2
Prisky15 <priskyihex15@gmail.com>
2023-03-22 01:19:12 -0700

    6-abs-update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _abs - a function that computes the absolute value of an integer
- * @c: integer input
- * Return: absolute value of number or zero
- */
+* _abs - a function that computes the absolute value of an integer
+* @c: integer input
+* Return: absolute value of number or zero
+*/
 int _abs(int c)
 {
 	if (c < 0)
 	{
-		int abs_val;
+	int abs_val;
 
-		abs_val= c * -1;
-		return (abs_val);
+	abs_val= c * -1;
+	return (abs_val);
 	}
 	return (c);
 }
