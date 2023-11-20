e09b1386c7381978a5f3f81b6d16ff89c8030f0a
IHEKORONYE Priska <priskyihex15@gmail.com>
2023-11-20 01:42:28 -0800

    malloc free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,10 @@
+####[5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe](100-argstostr.c)
+
+- Write a function that concatenates all the arguments of your program.
+
+- Prototype: char *argstostr(int ac, char **av);
+- Returns NULL if ac == 0 or av == NULL
+- Returns a pointer to a new string, or NULL if it fails
+- Each argument should be followed by a \n in the new string  
+
+
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,9 @@
+####[3. If you even dream of beating me you'd better wake up and apologize](3-alloc_grid.c)
+
+- Write a function that returns a pointer to a 2 dimensional array of integers.
+
+- Prototype: int **alloc_grid(int width, int height);
+- Each element of the grid should be initialized to 0
+- The function should return NULL on failure
+- If width or height is 0 or negative, return NULL
+
